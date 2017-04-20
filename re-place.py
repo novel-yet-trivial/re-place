f9d0b40623e20978c9bf39b6ba86ee04159720ef
Simon <thatguywiththatname@users.noreply.github.com>
2017-04-20 20:34:29 +0100

    Added try/except for next()
    CHEATER -> STEP

--- re-place.py
+++ re-place.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
-from csv import DictReader
-import lzma
-import numpy as np
-import matplotlib.pyplot as plt
+print("Loading...")
 import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+from csv import DictReader
+import numpy as np
+import lzma
 
 HEIGHT, WIDTH = 1001, 1001
-CHEATER = 10000 # update display every n changed pixels. The higher this number the faster the animation
+STEP = 10000 # update display every n changed pixels
 
 colour_lookup = [
     (1.00, 1.00, 1.00), (0.89, 0.89, 0.89), (0.53, 0.53, 0.53), (0.13, 0.13, 0.13),
     (1.00, 0.65, 0.82), (0.90, 0.00, 0.00), (0.90, 0.58, 0.00), (0.63, 0.42, 0.26),
     (0.90, 0.85, 0.00), (0.58, 0.88, 0.27), (0.01, 0.75, 0.00), (0.00, 0.90, 0.94),
-    (0.00, 0.51, 0.78), (0.00, 0.00, 0.92), (0.88, 0.29, 1.00), (0.51, 0.00, 0.50)]
+    (0.00, 0.51, 0.78), (0.00, 0.00, 0.92), (0.88, 0.29, 1.00), (0.51, 0.00, 0.50)
+]
 
 print("Opening csv")
 csvfile = lzma.open('sorted_pixels.csv.xz', 'rt')
@@ -24,8 +26,12 @@ data = np.full((WIDTH, HEIGHT, 3), 1.0, dtype=np.float32)
 print("Done, Drawing")
 
 def draw(i):
-    for idx in range(CHEATER):
-        row = next(reader)
+    for idx in range(STEP):
+        try:
+            row = next(reader)
+        except StopIteration:
+            print("Finished")
+            break
         x = int(row["x_coordinate"])
         y = int(row["y_coordinate"])
         c = int(row["color"])
@@ -34,8 +40,6 @@ def draw(i):
     return im,
 
 fig = plt.figure()
-
 im = plt.imshow(data, animated=True)
-
 ani = animation.FuncAnimation(fig, draw, interval=50, blit=True)
 plt.show()
