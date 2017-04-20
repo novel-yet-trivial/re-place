378b2a9f82666fd0c9932e476d664ebf41ffebfe
nyt <jonsspamaccout@gmail.com>
2017-04-20 13:10:27 -0700

    remade data file in binary format

--- README.md
+++ README.md
@@ -2,15 +2,13 @@
 
 An attempt to re-create what happened at [r/place](https://www.reddit.com/r/place)
 
-The grid is 1001x1001 because each "pixel" is 1x1 and if a pixel is placed at
-(1000, 1000), due to the create_rectangle function, its bottom right corner
-would be located at (1001, 1001)
-
 The data file needed for this program (included) was taken from [here](https://mega.nz/#!E0gQ2KiY!n6HnDfR8bpQz79fKv4GtTt06S4iLSBq7bDfeo-rxuUU)
 , which is from [this post](https://www.reddit.com/r/place/comments/65x14m/place_time_lapse_and_data_from_start_to_finish/)
 .
 
-The structure of the data file is `(time_stamp, user_hash, x_coordinate, y_coordinate, color)`
+The binary data file uses 3 bytes (24 bits) per pixel. Each pixel is saved as a int24 little endian which is (10 bit uint X), (10 bit uint Y), (4 bit uint color).
+
+The data comprises 16,556,641 pixels changes. X and Y are in the range 0 - 999 (inclusive). color is in the range 0 - 15 (inclusive). 3,256 pixels were removed from the original dataset as they were out of range; presumibly due to data sanitation error.
 
 ---
 
--- re-place.py
+++ re-place.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 from csv import DictReader
-import lzma
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-HEIGHT, WIDTH = 1001, 1001
+HEIGHT, WIDTH = 1000, 1000
 CHEATER = 10000 # update display every n changed pixels. The higher this number the faster the animation
 
 colour_lookup = [
@@ -15,20 +14,26 @@ colour_lookup = [
     (0.90, 0.85, 0.00), (0.58, 0.88, 0.27), (0.01, 0.75, 0.00), (0.00, 0.90, 0.94),
     (0.00, 0.51, 0.78), (0.00, 0.00, 0.92), (0.88, 0.29, 1.00), (0.51, 0.00, 0.50)]
 
-print("Opening csv")
-csvfile = lzma.open('sorted_pixels.csv.xz', 'rt')
-print("Open, feeding to DictReader")
-reader = DictReader(csvfile)
+print("Opening binary file")
+fh = open('pixels.bin', 'rb')
 print("Making a million pixels")
 data = np.full((WIDTH, HEIGHT, 3), 1.0, dtype=np.float32)
 print("Done, Drawing")
 
+def from_bytes3(b):
+    '''3 bytes => 24 bit integer => x, y, and color'''
+    i = int.from_bytes(b, 'little')
+    return i >> 14 & 1023, i >> 4 & 1023, i & 15
+
 def draw(i):
     for idx in range(CHEATER):
-        row = next(reader)
-        x = int(row["x_coordinate"])
-        y = int(row["y_coordinate"])
-        c = int(row["color"])
+        pixel = fh.read(3)
+        if not pixel:
+            # file is exhausted. Restart.
+            data.fill(1.0) # clear the image
+            fh.seek(0) # restart the file
+            continue
+        x, y, c = from_bytes3(pixel)
         data[y][x] = colour_lookup[c]
     im.set_array(data)
     return im,
